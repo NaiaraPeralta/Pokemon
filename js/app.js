b9f38962486a7b3c019200f8cbec2eb31af6e93d
Francisco Garcia Gomez <152178489+FrankTrabajo@users.noreply.github.com>
2024-10-29 20:45:25 +0100

    separaciones js cada tarea

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 let pokemons= pokemon;
 let añadir= window.localStorage.setItem("pokemons", JSON.stringify(pokemons));
 //JSON.parse (window.localStorage.getItem("pokemons"));
+
+
+
+// INDEX
+
+
+
+
+
+
+// CARDS
+
+
+
+
+
+// AGREGAR
