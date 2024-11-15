347f58f80238f398235afebeebe5327c2edf95aa
NaiaraPeralta <naiaraperacev@gmail.com>
2024-11-15 19:37:16 +0100

    v1.7.6 id arreglado

--- js/agregar.js
+++ js/agregar.js
@@ -1,86 +1,86 @@
 //El DOMContentLoaded ocurre cuando el documento ha sido completamente cargado, es decir para cuando se ha terminado
 //de ejecutar todo el codigo.
 document.addEventListener("DOMContentLoaded", () => {
-    let main = document.getElementsByTagName('main');
-    //Div del formulario
-    let divFormAgregar = document.createElement('div');
-    // Crear la etiqueta de H2 
-    let title = document.createElement("h2");
-    title.textContent = "Agregar un nuevo Pokémon";
-    title.classList.add('h2StyleAgregar');
-    divFormAgregar.appendChild(title);
-    //  Formulario
-    //creo la etiqueta form
-    let forms = document.getElementsByTagName('form');
-    let form = forms[0];
-    //le asigno un id al formulario para que si luego necesitamos llamarlo para cualquier otra fucnion podamos llamarlo
-    //por el id
-    form.id = "formularioPokemon";
-    form.method = "get";
-  
-  
-    // Creo el label para el nombre
-    let labelNb = document.createElement("label");
-    labelNb.textContent = "Nombre: ";
-    labelNb.classList.add('datos');
-    //creo el input para el nombre
-    let inputNb = document.createElement("input");
-    //especifico que el input es de tipo texto
-    inputNb.type = "text";
-    inputNb.id = "nombre";
-    inputNb.name = "nombre";
-    //para ponerlo como campo obligatorio a rellenar 
-    inputNb.required = true;
-    //Agrega un nuevo nodo como último hijo de un elemento.(explicacion profe), 
-    //es decir añadimos un nuevo nodo al final del formulario 
-    labelNb.appendChild(inputNb);
-    form.appendChild(labelNb);
-    //creo el br
+  let main = document.getElementsByTagName('main');
+  //Div del formulario
+  let divFormAgregar = document.createElement('div');
+  // Crear la etiqueta de H2 
+  let title = document.createElement("h2");
+  title.textContent = "Agregar un nuevo Pokémon";
+  title.classList.add('h2StyleAgregar');
+  divFormAgregar.appendChild(title);
+  //  Formulario
+  //creo la etiqueta form
+  let forms = document.getElementsByTagName('form');
+  let form = forms[0];
+  //le asigno un id al formulario para que si luego necesitamos llamarlo para cualquier otra fucnion podamos llamarlo
+  //por el id
+  form.id = "formularioPokemon";
+  form.method = "get";
+
+
+  // Creo el label para el nombre
+  let labelNb = document.createElement("label");
+  labelNb.textContent = "Nombre: ";
+  labelNb.classList.add('datos');
+  //creo el input para el nombre
+  let inputNb = document.createElement("input");
+  //especifico que el input es de tipo texto
+  inputNb.type = "text";
+  inputNb.id = "nombre";
+  inputNb.name = "nombre";
+  //para ponerlo como campo obligatorio a rellenar 
+  inputNb.required = true;
+  //Agrega un nuevo nodo como último hijo de un elemento.(explicacion profe), 
+  //es decir añadimos un nuevo nodo al final del formulario 
+  labelNb.appendChild(inputNb);
+  form.appendChild(labelNb);
+  //creo el br
+  form.appendChild(document.createElement("br"));
+  form.appendChild(document.createElement("br"));
+
+  let estadisticasBase = ["HP", "Ataque", "Defensa", "Ataque Especial", "Defensa Especial", "Velocidad"];
+
+  //recorro el array y creo q el label y el imput para las estadisticas base
+  for (let i in estadisticasBase) {
+    let labelEB = document.createElement("label");
+    labelEB.classList.add('datos');
+    //aqui utilizo las comillas invertidas para q lo de hp , ataque ... se me muestre como texto 
+    //porque si lo pongo con comillas " " se pone como cadena de texto literal y no funciona 
+    labelEB.textContent = estadisticasBase[i] + ": ";
+    let inputEB = document.createElement("input");
+    inputEB.type = "number";
+    inputEB.id = estadisticasBase[i];
+    inputEB.name = estadisticasBase[i];
+    inputEB.required = true;
+    labelEB.appendChild(inputEB);
+    form.appendChild(labelEB);
     form.appendChild(document.createElement("br"));
     form.appendChild(document.createElement("br"));
-  
-    let estadisticasBase = ["HP", "Ataque", "Defensa", "Ataque Especial", "Defensa Especial", "Velocidad"];
+  }
 
-    //recorro el array y creo q el label y el imput para las estadisticas base
-    for(let i in estadisticasBase) {
-      let labelEB = document.createElement("label");
-      labelEB.classList.add('datos');
-      //aqui utilizo las comillas invertidas para q lo de hp , ataque ... se me muestre como texto 
-      //porque si lo pongo con comillas " " se pone como cadena de texto literal y no funciona 
-      labelEB.textContent = estadisticasBase[i] + ": ";
-      let inputEB = document.createElement("input");
-      inputEB.type = "number";
-      inputEB.id = estadisticasBase[i];
-      inputEB.name = estadisticasBase[i];
-      inputEB.required = true;
-      labelEB.appendChild(inputEB);
-      form.appendChild(labelEB);
-      form.appendChild(document.createElement("br"));
-      form.appendChild(document.createElement("br"));
-    }
-  
-    form.appendChild(document.createElement("br"));
+  form.appendChild(document.createElement("br"));
 
-    
 
-    let inputboton = document.createElement("input");
-    inputboton.type = "submit";
-    inputboton.id = "agregar";
-    inputboton.value = "Agregar Pokémon";
-    form.appendChild(inputboton);
 
-    divFormAgregar.appendChild(form);
-    divFormAgregar.classList.add("divFormularioAgregar");
-    main[0].appendChild(divFormAgregar);
-    main[0].appendChild(forms);
-   
-/** 
-    // Creo el boton de agregar 
-    let botonAgregar = document.createElement("button");
-    botonAgregar.type = "submit";
-    botonAgregar.textContent = "Agregar Pokémon";
-    form.appendChild(botonAgregar);
-  */
+  let inputboton = document.createElement("input");
+  inputboton.type = "submit";
+  inputboton.id = "agregar";
+  inputboton.value = "Agregar Pokémon";
+  form.appendChild(inputboton);
+
+  divFormAgregar.appendChild(form);
+  divFormAgregar.classList.add("divFormularioAgregar");
+  main[0].appendChild(divFormAgregar);
+  main[0].appendChild(forms);
+
+  /** 
+      // Creo el boton de agregar 
+      let botonAgregar = document.createElement("button");
+      botonAgregar.type = "submit";
+      botonAgregar.textContent = "Agregar Pokémon";
+      form.appendChild(botonAgregar);
+    */
 
 
 });
@@ -88,12 +88,12 @@ document.addEventListener("DOMContentLoaded", () => {
 let datosUrl = window.location.search;
 let datos1 = datosUrl.split('&');
 let arrDatos = [];
-for(let i = 0 ; i < datos1.length ; i++){
-    arrDatos.push(datos1[i].split('='));
+for (let i = 0; i < datos1.length; i++) {
+  arrDatos.push(datos1[i].split('='));
 }
 let datosDefin = [];
-for(let i in arrDatos){
-    datosDefin.push(arrDatos[i][1]);
+for (let i in arrDatos) {
+  datosDefin.push(arrDatos[i][1]);
 }
 let nombre = datosDefin[0];
 let hp = datosDefin[1];
@@ -111,33 +111,35 @@ console.log(ataqueEspecial);
 console.log(defensaEspecial);
 console.log(velocidad);
 
-function agregarPokemon(nombre,hp,ataque,defensa,ataqueEspecial,defensaEspecial,velocidad) {
+function agregarPokemon(nombre, hp, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad) {
   let id;
   let pokemon = JSON.parse(window.localStorage.getItem("pokemons"));
   let pokemonLocal = JSON.parse(window.localStorage.getItem("pokemonsAgregado"));
-  id = pokemon.length+1;
-  id++;
- 
+  id = pokemon.length + 1;
+  idAgregado = pokemonLocal.length + id;
+
+
   //creo un nuevoPokemon con los datos que meto en el formulario 
   let nuevoPokemon = {
-    id : id,
+    id: idAgregado,
     nombre: nombre,
     estadisticas_base: {
       hp: hp,
-      ataque:ataque,
+      ataque: ataque,
       defensa: defensa,
       ataque_especial: ataqueEspecial,
       defensa_especial: defensaEspecial,
       velocidad: velocidad
     }
   }
-// agregar pokemon
+  // agregar pokemon
 
-pokemonLocal.push(nuevoPokemon); 
-window.localStorage.setItem('pokemonsAgregado',JSON.stringify(pokemonLocal));
+  pokemonLocal.push(nuevoPokemon);
+  window.localStorage.setItem('pokemonsAgregado', JSON.stringify(pokemonLocal));
+  
 }
 
-if(nombre && hp && ataque && defensa && ataqueEspecial && defensaEspecial && velocidad){
+if (nombre && hp && ataque && defensa && ataqueEspecial && defensaEspecial && velocidad) {
   agregarPokemon(nombre, parseInt(hp), parseInt(ataque), parseInt(defensa), parseInt(ataqueEspecial), parseInt(defensaEspecial), parseInt(velocidad));
 }
 
@@ -180,13 +182,13 @@ nav[0].appendChild(divRedirect);
 // *************  Footer  ****************
 //Primero un footer sencillo
 let footer = document.getElementsByTagName('footer');
-    // En el footer crearemos un pequeño div, para tenerlo todo mas organizado
+// En el footer crearemos un pequeño div, para tenerlo todo mas organizado
 let footerDiv = document.createElement('div');
-    // Agregamos unos pocos comentarios para que el footer sea mas "bonito"
+// Agregamos unos pocos comentarios para que el footer sea mas "bonito"
 let pComentariosFooterDiv = document.createElement('p');
 pComentariosFooterDiv.textContent = "Proyecto DOM 3 - Pokedex";
 let hrFooter = document.createElement('hr');
-    // Le agregaremos un pequeño texto diciendo quienes han sido los autores de este proyecto
+// Le agregaremos un pequeño texto diciendo quienes han sido los autores de este proyecto
 let pAutoresFooterDiv = document.createElement('p');
 pAutoresFooterDiv.textContent = "Proyecto creado por: Naiara Peralta Acevedo, Laura García Yuste, Francisco Garcia Gomez.";
 
@@ -198,4 +200,3 @@ footer[0].appendChild(footerDiv);
 
 
 
-  
